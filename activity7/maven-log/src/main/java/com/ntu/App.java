1f2e7bcac8cd79fe454b8808d64700eb292dae08
Justen Manni <me@justenmanni.dev>
2023-08-15 19:33:59 +0800

    Complete Maven Log

---
+++ activity7/maven-log/src/main/java/com/ntu/App.java
@@ -0,0 +1,21 @@
+package com.ntu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        logger.info("Application started");
+        logger.info("This is an informational message.");
+        logger.error("This is an error message.");
+        logger.warn("This is a warning message.");
+        logger.info("Application ended");
+    }
+}
