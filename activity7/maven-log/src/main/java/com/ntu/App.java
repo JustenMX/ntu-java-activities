7d3dc3d9a9493867327e174daece95daf409a2bd
Justen Manni <me@justenmanni.dev>
2023-08-16 14:53:32 +0800

    Update Changes

--- activity7/maven-log/src/main/java/com/ntu/App.java
+++ activity7/maven-log/src/main/java/com/ntu/App.java
@@ -1,5 +1,11 @@
 package com.ntu;
 
+//
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+//
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,11 +17,31 @@ public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         logger.info("Application started");
-        logger.info("This is an informational message.");
-        logger.error("This is an error message.");
-        logger.warn("This is a warning message.");
-        logger.info("Application ended");
+        try {
+            readFile("activity7/maven-log/src/main/java/com/ntu/greet.txt");
+        } catch (IOException exception) {
+            logger.error(" 🔴 An error occurred", exception.getMessage());
+        } finally {
+            logger.info("🟢 Process Complete");
+        }
+        // System.out.println("Hello World!");
+        // logger.info("Application started");
+        // logger.info("This is an informational message.");
+        // logger.error("This is an error message.");
+        // logger.warn("This is a warning message.");
+        // logger.info("Application ended");
     }
+
+    public static void readFile(String fileName) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream("activity7/maven-log/src/main/java/com/ntu/greet.txt");
+        Scanner scanner = new Scanner(fis);
+        if (scanner.hasNextLine()) {
+            logger.info("🟢 File Read");
+            System.out.println(scanner.nextLine());
+        }
+        scanner.close();
+
+    }
+
 }
